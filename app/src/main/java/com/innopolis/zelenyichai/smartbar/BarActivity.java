93f11e1c8a88124c667dc395e8b1fa368a0f362e
Arthur Aukhatov <aaukhatov@gmail.com>
2018-04-07 22:33:17 +0300

    bar's map

--- app/src/main/java/com/innopolis/zelenyichai/smartbar/BarActivity.java
+++ app/src/main/java/com/innopolis/zelenyichai/smartbar/BarActivity.java
@@ -1,7 +1,7 @@
 package com.innopolis.zelenyichai.smartbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class BarActivity extends AppCompatActivity {
 
