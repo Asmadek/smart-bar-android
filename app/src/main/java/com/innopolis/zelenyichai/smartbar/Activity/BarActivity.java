3884fe9eefe1ce5b0fccf970c60b24f16076f299
Arthur Aukhatov <aaukhatov@gmail.com>
2018-04-08 07:13:42 +0300

    список баров, без отображения

--- app/src/main/java/com/innopolis/zelenyichai/smartbar/Activity/BarActivity.java
+++ app/src/main/java/com/innopolis/zelenyichai/smartbar/Activity/BarActivity.java
@@ -12,12 +12,30 @@ import com.innopolis.zelenyichai.smartbar.Fragment.ChatFragment;
 import com.innopolis.zelenyichai.smartbar.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class BarActivity extends AppCompatActivity implements View.OnClickListener{
+public class BarActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Bundle bundle;
     private ChatFragment chatFragment;
     private Button map, list;
+    private static List<String> barsMock = new ArrayList<>();
+
+    static {
+        barsMock.add("Rockstar Bar");
+        barsMock.add("Twin Peaks");
+        barsMock.add("Beer House");
+        barsMock.add("Trinity Irish Pub");
+        barsMock.add("Cuba Libre");
+        barsMock.add("IQ Bar");
+        barsMock.add("Fomin");
+        barsMock.add("Соль");
+        barsMock.add("Бельгиан");
+        barsMock.add("Что делать!?");
+        barsMock.add("Jam Bar");
+        barsMock.add("Дублин");
+        barsMock.add("Craft House");
+        }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +60,11 @@ public class BarActivity extends AppCompatActivity implements View.OnClickListen
         switch (v.getId()){
             case R.id.btn_list:
                 findViewById(R.id.image_barmap).animate().alpha(0.0f).setDuration(500);
+                break;
+
+            case R.id.btn_map:
+                // TODO отобразить список баров
+                break;
         }
     }
 }
